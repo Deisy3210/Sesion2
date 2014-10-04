7dbcaadc61a7264b1bed19df130cd807b1125496
Deisy3210 <deisy.leal3210@gmail.com>
2014-10-04 10:16:06 -0500

    Sesion2
    Curso.Net

---
+++ AppSwitch2/AppSwitch2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppSwitch2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String numero;
+
+            Console.WriteLine("Ingrese un numero entre 1 y 5");
+            numero = Console.ReadLine();
+
+            switch (numero)
+            {
+                case "1":
+                    Console.WriteLine("UNO");
+                    break;
+
+                case "2":
+                    Console.WriteLine("DOS");
+                    break;
+
+                case "3":
+                    Console.WriteLine("TRES");
+                    break;
+
+                case "4":
+                    Console.WriteLine("CUATRO");
+                    break;
+
+                case "5":
+                    Console.WriteLine("CINCO");
+                    break;
+
+                default:
+                    Console.WriteLine("Numero fuera de rango");
+                    break;
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ AppnLongitudNumero/AppnLongitudNumero/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppnLongitudNumero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String numero;
+            int num;
+            Console.WriteLine("Encontrar longitud de numero");
+            Console.WriteLine("Digite un numero entre 1 y 99:");
+            numero = Console.ReadLine();
+            num = int.Parse(numero);
+            if(numero.Length<=2 && num>0)
+            {
+                Console.WriteLine("El numero es positivo");
+                if (numero.Length == 2)
+                {
+                    Console.WriteLine("El numero tiene dos digitos");
+                }
+                else
+                {
+                    Console.WriteLine("El numero tiene un digito");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Numero fuera de rango o es negativo");
+            }
+            Console.ReadKey();
+        }
+    }
+}
